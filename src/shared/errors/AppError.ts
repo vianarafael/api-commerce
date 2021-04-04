cf7807c0100cfdd246099807aa845853e90f2db5
vianarafael <vianarafaelds@gmail.com>
2021-04-05 05:55:17 +0900

    created an error class & middleware

---
+++ src/shared/errors/AppError.ts
@@ -0,0 +1,15 @@
+import { stat } from "node:fs";
+
+class AppError
+{
+  public readonly message: string;
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode = 400)
+  {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default AppError;
