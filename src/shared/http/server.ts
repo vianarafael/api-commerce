56cb5d32c2b52248916ad35aa2531dc54304d1ec
vianarafael <vianarafaelds@gmail.com>
2021-04-06 05:40:31 +0900

    migrations config

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,33 @@
-import express from 'express'
+import 'reflect-metadata'
+import express, { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 import routes from './routes'
+import AppError from '@shared/errors/AppError'
+import '@shared/typeorm'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
 app.use(routes)
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) =>
+{
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  } else
+  {
+    response.status(500).json({
+      status: 'error',
+      message: 'Internal server error'
+    })
+  }
+})
+
 
 const port = 3333
 app.listen(port, () =>
