96f9f857ba69df6af895caba09a1bc8fad94c6bc
vianarafael <vianarafaelds@gmail.com>
2021-04-08 07:05:06 +0900

    finished the product routes, pointed the entities folder in ormconfig

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
+import productsRouter from '@modules/products/routes/products.routes'
 
 const routes = Router()
-routes.get('/', (request, response) =>
-{
-  response.json({ message: "Hello, dev" })
-})
+
+routes.use('/products', productsRouter);
+
 
 export default routes
